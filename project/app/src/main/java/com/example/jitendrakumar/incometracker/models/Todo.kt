8c27a35a226a9af84787e1486c55fda9a576476e
reocoder <mukherjeeshreoshee@gmail.com>
2021-08-19 20:22:56 +0530

    updated

--- project/app/src/main/java/com/example/jitendrakumar/incometracker/models/Todo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jitendrakumar.incometracker.models
-
-data class Todo (
-    val id: Int?,
-    val taskName: String,
-    val taskDate: String,
-    val taskTime: String,
-    var done: Boolean
-)
--- project/app/src/main/java/com/example/jitendrakumar/incometracker/validations/SignupValidation.java
+++
@@ -1,4 +0,0 @@
-package com.example.jitendrakumar.incometracker.validations;
-
-public class SignupValidation {
-}
